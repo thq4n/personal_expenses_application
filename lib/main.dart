cbe253336df69a30be0d4151328cf645f64ab605
Quan Gian <82435818+thq4n@users.noreply.github.com>
2021-06-28 14:17:01 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("My App"),
+        title: Text("My Appp"),
         actions: <Widget>[
           IconButton(
             onPressed: () => _startAddNewTransation(context),
