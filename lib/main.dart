f00a6fc582e1d40acb223fef8f9381a6c9c90697
Quan Gian <82435818+thq4n@users.noreply.github.com>
2021-06-28 18:35:32 +0700

    Chart_1

--- lib/main.dart
+++ lib/main.dart
@@ -89,7 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter App"),
+        title: Text("My App"),
         actions: <Widget>[
           IconButton(
             onPressed: () => _startAddNewTransation(context),
