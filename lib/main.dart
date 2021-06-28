a866f4ab0d029c1e92ead987c9c4a938cab66cc2
Quan Gian <82435818+thq4n@users.noreply.github.com>
2021-06-28 14:14:33 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Apppp',
+      title: 'My App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: "Quicksand",
